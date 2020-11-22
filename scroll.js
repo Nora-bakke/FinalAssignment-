15d729f3a25e834979b97123491ffa6134b0ae11
Amitaf1 <70072602+Amitaf1@users.noreply.github.com>
2020-11-22 16:16:12 +0100

    js file???

--- scroll.js
+++ scroll.js
@@ -1,22 +1,22 @@
 function smoothScroll(target, duration) {
   var target = document.querySelector(target);
   var targetPosition = target.getBoundingClientRect().top;
-  var startingPosition = window.pageYOffset();
-  var distance = targetPosition - startingPosition;
+  var startPosition = window.pageYOffset();
+  var distance = targetPosition - startPosition;
   var startTime = null;
 
-  function scrollAnimation(currentTime) {
+  function animation(currentTime) {
     if (startTime === null) {
       startTime = currentTime;
     }
     var timeElapsed = currentTime - startTime;
-    var run = easeIn(timeElapsed, startingPosition, distance, duration);
+    var run = ease(timeElapsed, startPosition, distance, duration);
     window.scrollTo(0, run);
     if (timeElapsed < duration) {
       requestAnimationFrame(animation);
     }
   }
-  function easeIn (a, b, c, d) {
+  function ease (t, b, c, d) {
     t /= d / 2;
     if (t < 1) {
       return c / 2 * t * t + b;
@@ -28,7 +28,8 @@ function smoothScroll(target, duration) {
 }
 
 
-var scroll = document.querySelector('scroll');
-scroll.addEventListener('click'function() {
-  smoothScroll('#scroll', 8000);
+var scroll = document.querySelector('.scroll');
+
+scroll.addEventListener('click',function() {
+  smoothScroll('.smoothie-text', 1000);
 });
